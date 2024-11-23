54740d7027a704669bb24dc161557bb4a1632bf2
Trevor Dolby <trevor.dolby@ibm.com>
2024-11-23 15:02:07 -0600

    Git PR 1
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- scripts/push-changes-to-new-branch.sh
+++ scripts/push-changes-to-new-branch.sh
@@ -21,12 +21,18 @@ if [[ $(git status -s | wc -l) -eq 0 ]]; then
   echo "########################################################################"
 else
   echo "########################################################################"
-  echo "# Generated files changed - creating and pushing git commit:"
-  echo "# \"$MSG\""
+  echo "# Generated files changed - creating and pushing git commit"
+  echo "# \"$MSG\" to branch $BRANCH"
   echo "########################################################################"
-  git checkout -b $BRANCH
+  git checkout $BRANCH 2>/dev/null || git checkout -b $BRANCH
   git add . 
   git status
   git commit -s -m "$MSG"
   git push origin $BRANCH
+
+  echo "########################################################################"
+  echo "# Creating or updating PR"
+  echo "########################################################################"
+  gh pr list
+  gh pr create --base main --title "tea-ace-demo automated promote to $destEnv" --body "Updated on $DATE"  --draft || gh pr edit --base main --title "tea-ace-demo automated promote to $destEnv" --body "Updated on $DATE"
 fi
